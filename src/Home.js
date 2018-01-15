58c5d3646b55e81474b574be3f4bed32e20b1c4d
Weston Gibler <westongibler@gmail.com>
2018-01-15 08:10:59 -0700

    Base Boiler Ready

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to my page!</h2>
+        <p>Sample 1: My name is Randy Patterson, and I’m currently looking for a job in youth services. I have 10 years of experience working with youth agencies. I have a bachelor’s degree in outdoor education. I raise money, train leaders, and organize units. I have raised over $100,000 each of the last six years. I consider myself a good public speaker, and I have a good sense of humor. “Who do you know who works with youth?</p>
+        <p>Sample 2: My name is Lucas Martin, and I enjoy meeting new people and finding ways to help them have an uplifting experience. I have had a variety of customer service opportunities, through which I was able to have fewer returned products and increased repeat customers, when compared with co-workers. I am dedicated, outgoing, and a team player. Who could I speak with in your customer service department about your organization’s customer service needs?</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/Stuff.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>STUFF</h2>
+        <p>Lets talk about stuff I am good at:</p>
+        <ol>
+          <li>Jumping Jacks</li>
+          <li>Eating Spicy Foodz</li>
+          <li>Silent Farting</li>
+          <li>False Starting, unnoticed</li>
+          <li>Petting Dogs</li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default Stuff;
